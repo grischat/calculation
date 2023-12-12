0cbecf516ae869f8f104b2eb95a2a949126aea89
grischat <kosolapg2@gmail.com>
2023-12-12 16:56:41 +0100

    Changed UI + added field -Changed UI of inputs -Added Exec rate field

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import "./App.scss";
 function App() {
   const formik = useFormik({
     initialValues: {
-      totalUsers: "",
-      usersPerExec: "",
-      execRatePerHour: "",
-      execRatePerDay: "",
+      totalItems: "274000",
+      itemsPerExec: "20",
+      execRatePerHour: "30",
+      execRatePerDay: "8",
       totalRuns: "",
       runsPerDay: "",
       totalDays: "",
+      totalExecs: "",
     },
     onSubmit: (values, { setSubmitting }) => {
       const jsonText = JSON.stringify(values, null, 2);
@@ -22,18 +23,31 @@ function App() {
   });
   useEffect(() => {
     const totalRuns = Math.ceil(
-      formik.values.totalUsers / formik.values.usersPerExec
+      formik.values.totalItems / formik.values.itemsPerExec
     );
     const runsPerDay = Math.ceil(
       formik.values.execRatePerHour * formik.values.execRatePerDay
     );
     const totalDays = Math.ceil(totalRuns / runsPerDay);
+
+    const totalExecs = Math.ceil(60 / formik.values.execRatePerHour);
+
+    const execRatePerDay = parseFloat(formik.values.execRatePerDay);
+    if (isNaN(execRatePerDay) || execRatePerDay < 0 || execRatePerDay > 24) {
+      formik.setFieldError(
+        "execRatePerDay",
+        "Invalid value. Must be between 0 and 24."
+      );
+      formik.setFieldValue("execRatePerDay", "No days added");
+      return;
+    }
     formik.setFieldValue("totalRuns", totalRuns);
     formik.setFieldValue("runsPerDay", runsPerDay);
     formik.setFieldValue("totalDays", totalDays);
+    formik.setFieldValue("totalExecs", totalExecs);
   }, [
-    formik.values.totalUsers,
-    formik.values.usersPerExec,
+    formik.values.totalItems,
+    formik.values.itemsPerExec,
     formik.values.execRatePerHour,
     formik.values.execRatePerDay,
   ]);
@@ -41,54 +55,62 @@ function App() {
   return (
     <>
       <form id="form" onSubmit={formik.handleSubmit}>
-        <label htmlFor="totalUsers">
-          Total users (user)
-          <input
-            id="totalUsers"
-            name="totalUsers"
-            className="input"
-            type="number"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.totalUsers}
-          />
-        </label>
-        <label htmlFor="usersPerExec">
-          Users per execution (user/exec)
-          <input
-            id="usersPerExec"
-            name="usersPerExec"
-            className="input"
-            type="number"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.usersPerExec}
-          />
-        </label>
-        <label htmlFor="execRatePerHour">
-          Execution rate per hour (exec/hour)
-          <input
-            id="execRatePerHour"
-            name="execRatePerHour"
-            className="input"
-            type="number"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.execRatePerHour}
-          />
-        </label>
-        <label htmlFor="execRatePerDay">
-          Execution rate per day (exec/day)
-          <input
-            id="execRatePerDay"
-            name="execRatePerDay"
-            className="input"
-            type="number"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.execRatePerDay}
-          />
-        </label>
+        <div className="values__container">
+          <div className="label-input-container">
+            <label htmlFor="totalItems">Total items </label>
+            <input
+              id="totalItems"
+              name="totalItems"
+              className="input"
+              type="number"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.totalItems}
+            />
+            <span>(item)</span>
+          </div>
+          <div className="label-input-container">
+            <label htmlFor="itemsPerExec">Items per execution</label>
+            <input
+              id="itemsPerExec"
+              name="itemsPerExec"
+              className="input"
+              type="number"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.itemsPerExec}
+            />
+            <span> (item/exec)</span>
+          </div>
+          <div className="label-input-container">
+            <label htmlFor="execRatePerHour">Execution rate per hour</label>
+            <input
+              id="execRatePerHour"
+              name="execRatePerHour"
+              className="input"
+              type="number"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.execRatePerHour}
+            />
+            <span>(exec/hour)</span>
+          </div>
+          <div className="label-input-container">
+            <label htmlFor="execRatePerDay">Execution rate per day</label>
+            <input
+              id="execRatePerDay"
+              name="execRatePerDay"
+              className="input"
+              type="number"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.execRatePerDay}
+              min="0"
+              max="24"
+            />
+            <span>(exec/day)</span>
+          </div>
+        </div>
         <div className="results__container">
           <label>
             Total runs (exec)
@@ -106,6 +128,12 @@ function App() {
             Total days (day)
             <p>
               {isNaN(formik.values.totalDays) ? "0" : formik.values.totalDays}
+            </p>
+          </label>
+          <label>
+            Execution rate (exec/minute)
+            <p>
+              {isNaN(formik.values.totalExecs) ? "0" : formik.values.totalExecs}
             </p>
           </label>
         </div>
